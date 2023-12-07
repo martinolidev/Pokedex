04dfd521965d6f96560e06f325f78cd6c9412c08
Martin Martinez <mtz.martin@proton.me>
2023-12-07 11:23:44 -0600

    Preparing the view for the details of the pokemon

---
+++ Pokedex/PokemonDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  PokemonDetailView.swift
+//  Pokedex
+//
+//  Created by Martin Martinez on 07/12/23.
+//
+
+import SwiftUI
+
+struct PokemonDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PokemonDetailView()
+}
