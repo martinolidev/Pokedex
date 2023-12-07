f69a2abdf8812721d74aebab3ae17a5baa8979e2
Martin Martinez <mtz.martin@proton.me>
2023-12-07 15:10:37 -0600

    General information added :D

--- Pokedex/PokemonDetailView.swift
+++ Pokedex/PokemonDetailView.swift
@@ -82,6 +82,26 @@ struct PokemonDetailView: View {
                     .bold()
                     .font(.system(size: 30))
                 
+                RoundedRectangle(cornerRadius: 16)
+                    .frame(width: 410, height: 120)
+                    .overlay(
+                        VStack(alignment: .leading) {
+                            HStack(spacing: 50) {
+                                Text("Height: ")
+                                Text("1,2m")
+                                    .foregroundStyle(Color.black)
+                            }
+                            HStack(spacing: 50) {
+                                Text("Weight: ")
+                                Text("54,0Kg")
+                                    .foregroundStyle(Color.black)
+                            }
+                        }
+                            .font(.system(size: 30))
+                            .foregroundStyle(Color.white)
+                            .bold()
+                    )
+                    .foregroundStyle(Color.cyan)
             }
         }
         .padding()
