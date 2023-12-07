7cdb8f69890b5aee64df45683df9d435875a054c
Martin Martinez <mtz.martin@proton.me>
2023-12-07 11:24:07 -0600

    Pokemon detailed view

--- Pokedex/PokemonDetailView.swift
+++ Pokedex/PokemonDetailView.swift
@@ -9,7 +9,14 @@ import SwiftUI
 
 struct PokemonDetailView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack(spacing: 50) {
+                Text("Lucario")
+                Text("N.° 0448")
+            }
+            .bold()
+            .font(.system(size: 30))
+        }
     }
 }
 
