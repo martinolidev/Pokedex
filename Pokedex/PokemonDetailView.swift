08d7ae396da30e9d7b04d9aaa8646e02e8d5c04b
Martin Martinez <mtz.martin@proton.me>
2023-12-07 11:40:22 -0600

    Adding more details

--- Pokedex/PokemonDetailView.swift
+++ Pokedex/PokemonDetailView.swift
@@ -16,7 +16,42 @@ struct PokemonDetailView: View {
             }
             .bold()
             .font(.system(size: 30))
+            
+            RoundedRectangle(cornerRadius: 16)
+                .overlay(
+                    HStack(spacing: 20) {
+                        Image("placeholder")
+                            .resizable()
+                            .foregroundColor(.white)
+                            .frame(width: 100, height: 100)
+                        Text("Dicen que, como es capaz de detectar auras, puede percibir a sus rivales aunque no los vea.")
+                            .foregroundColor(.white)
+                            .bold()
+                            .font(.system(size: 17))
+                    }.padding()
+                )
+                .frame(height: 150)
+                .foregroundColor(.red)
+            
+            RoundedRectangle(cornerRadius: 16)
+                .overlay(
+                    HStack {
+                        VStack {
+                            Text("Height")
+                                .foregroundStyle(Color.white)
+                            Text("1,2m")
+                                .foregroundStyle(Color.black)
+                            
+                            Text("Weight")
+                                .foregroundStyle(Color.white)
+                            Text("54,0 kg")
+                                .foregroundStyle(Color.black)
+                        }
+                    }
+                )
+                .foregroundColor(.blue)
         }
+        .padding()
     }
 }
 
