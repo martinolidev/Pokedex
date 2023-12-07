8374182b9b115158040ab5995285b320dce3196e
Martin Martinez <mtz.martin@proton.me>
2023-12-07 16:05:19 -0600

    Now finally the code for my genders view

---
+++ Pokedex/Views/GenderView.swift
@@ -0,0 +1,18 @@
+//
+//  GenderView.swift
+//  Pokedex
+//
+//  Created by Martin Martinez on 07/12/23.
+//
+
+import SwiftUI
+
+struct GenderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GenderView()
+}
