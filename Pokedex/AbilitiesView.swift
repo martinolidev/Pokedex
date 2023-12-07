49788cabcd36d072cb47b43243aafff16722197d
Martin Martinez <mtz.martin@proton.me>
2023-12-07 16:00:00 -0600

    Almost ready

--- Pokedex/AbilitiesView.swift
+++ Pokedex/AbilitiesView.swift
@@ -9,7 +9,29 @@ import SwiftUI
 
 struct AbilitiesView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            let abilities = ["Steadfast", "Inner-focus", "Justified"]
+            Text("Abilities")
+                .font(.system(size: 30))
+                .bold()
+            
+            ScrollView(.horizontal) {
+                HStack(spacing: 20) {
+                    ForEach(abilities, id: \.self) {
+                        abilities in
+                        RoundedRectangle(cornerRadius: 16)
+                            .frame(width: 150, height: 50)
+                            .overlay(
+                                Text(abilities)
+                                    .foregroundStyle(Color.white)
+                                    .bold()
+                            )
+                            .foregroundStyle(Color.red)
+                    }
+                }
+                .font(.system(size: 25))
+            }
+        }
     }
 }
 
--- Pokedex/PokemonDetailView.swift
+++ Pokedex/PokemonDetailView.swift
@@ -10,29 +10,7 @@ import SwiftUI
 struct PokemonDetailView: View {
     var body: some View {
         VStack {
-            VStack {
-                let abilities = ["Steadfast", "Inner-focus", "Justified"]
-                Text("Abilities")
-                    .font(.system(size: 30))
-                    .bold()
-                
-                ScrollView(.horizontal) {
-                    HStack(spacing: 20) {
-                        ForEach(abilities, id: \.self) {
-                            abilities in
-                            RoundedRectangle(cornerRadius: 16)
-                                .frame(width: 150, height: 50)
-                                .overlay(
-                                    Text(abilities)
-                                        .foregroundStyle(Color.white)
-                                        .bold()
-                                )
-                                .foregroundStyle(Color.red)
-                        }
-                    }
-                    .font(.system(size: 25))
-                }
-            }
+            
         }
         .padding()
     }
