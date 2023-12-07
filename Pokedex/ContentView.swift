dcdc18a5fc1cc484d81d5743acd31532725d351f
Martin Martinez <mtz.martin@proton.me>
2023-12-07 11:17:07 -0600

    Building the interface to search

--- Pokedex/ContentView.swift
+++ Pokedex/ContentView.swift
@@ -8,12 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var pokemonName = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Pokédex")
+                .font(.largeTitle)
+                .bold()
+            TextField("Search a Pokémon", text: $pokemonName)
+                .padding()
+                .border(Color.black, width: 1)
+                .cornerRadius(10)
+            Button {
+                //logc
+            } label: {
+                RoundedRectangle(cornerRadius: 16)
+                    .overlay(
+                        Text("Get")
+                            .foregroundColor(.white)
+                            .bold()
+                            .font(.system(size: 30))
+                    )
+                    .frame(width: 100, height: 50)
+                    .foregroundColor(.red)
+            }
         }
         .padding()
     }
