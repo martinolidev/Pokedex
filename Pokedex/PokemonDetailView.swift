ed4182764ad5b6c6c1ebe86d6c5c0554391e891d
Martin Martinez <mtz.martin@proton.me>
2023-12-07 16:01:48 -0600

    Where is my gender view? I have no idea :D I'll create it again

--- Pokedex/PokemonDetailView.swift
+++ Pokedex/PokemonDetailView.swift
@@ -10,6 +10,10 @@ import SwiftUI
 struct PokemonDetailView: View {
     var body: some View {
         VStack {
+            NameDescriptionView()
+            AbilitiesView()
+            TypesView()
+            GenInfoView()
             
         }
         .padding()
