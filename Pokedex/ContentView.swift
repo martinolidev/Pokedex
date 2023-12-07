a0d46e4f8f0dfef2fddefd4fca17a2253b885e3a
Martin Martinez <mtz.martin@proton.me>
2023-12-07 11:19:02 -0600

    Separate files

--- Pokedex/ContentView.swift
+++ Pokedex/ContentView.swift
@@ -12,26 +12,7 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            Text("Pokédex")
-                .font(.largeTitle)
-                .bold()
-            TextField("Search a Pokémon", text: $pokemonName)
-                .padding()
-                .border(Color.black, width: 1)
-                .cornerRadius(10)
-            Button {
-                //logc
-            } label: {
-                RoundedRectangle(cornerRadius: 16)
-                    .overlay(
-                        Text("Get")
-                            .foregroundColor(.white)
-                            .bold()
-                            .font(.system(size: 30))
-                    )
-                    .frame(width: 100, height: 50)
-                    .foregroundColor(.red)
-            }
+            SearchView()
         }
         .padding()
     }
--- Pokedex/SearchView.swift
+++ Pokedex/SearchView.swift
@@ -8,8 +8,29 @@
 import SwiftUI
 
 struct SearchView: View {
+    @State var pokemonName = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Pokédex")
+            .font(.largeTitle)
+            .bold()
+        TextField("Search a Pokémon", text: $pokemonName)
+            .padding()
+            .border(Color.black, width: 1)
+            .cornerRadius(10)
+        Button {
+            //logc
+        } label: {
+            RoundedRectangle(cornerRadius: 16)
+                .overlay(
+                    Text("Get")
+                        .foregroundColor(.white)
+                        .bold()
+                        .font(.system(size: 30))
+                )
+                .frame(width: 100, height: 50)
+                .foregroundColor(.red)
+        }
     }
 }
 
