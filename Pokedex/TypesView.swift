09456801fff3d31b1c1c4ddcfc808ebc9f042c75
Martin Martinez <mtz.martin@proton.me>
2023-12-07 15:53:49 -0600

    Copy pasty, becase i can :D

--- Pokedex/TypesView.swift
+++ Pokedex/TypesView.swift
@@ -9,7 +9,29 @@ import SwiftUI
 
 struct TypesView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Type")
+                .bold()
+                .font(.system(size: 30))
+            HStack {
+                RoundedRectangle(cornerRadius: 16)
+                    .foregroundColor(.red)
+                    .overlay(
+                        Text("Fight")
+                            .foregroundStyle(Color.white)
+                            .bold()
+                    )
+                    .frame(width: 80, height: 30)
+                RoundedRectangle(cornerRadius: 16)
+                    .foregroundStyle(Color.gray)
+                    .overlay(
+                        Text("Steel")
+                            .foregroundStyle(Color.white)
+                            .bold()
+                    )
+                    .frame(width: 80, height: 30)
+            }
+        }
     }
 }
 
