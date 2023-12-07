c4d8121f2023fa26b43ed13755c445f1397c83bd
Martin Martinez <mtz.martin@proton.me>
2023-12-07 11:08:22 -0600

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Martin Martinez on 07/12/23.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
