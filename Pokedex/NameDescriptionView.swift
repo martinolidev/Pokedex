f93585cd9ba4a0abc50fc88e203e3302c91e6288
Martin Martinez <mtz.martin@proton.me>
2023-12-07 15:50:44 -0600

    Oh i forgot to use the SwiftUI template, Is okay now

--- Pokedex/NameDescriptionView.swift
+++ Pokedex/NameDescriptionView.swift
@@ -5,4 +5,25 @@
 //  Created by Martin Martinez on 07/12/23.
 //
 
-import Foundation
+import SwiftUI
+
+struct NameDescriptionView: View {
+    var body: some View {
+        //name and id
+        HStack(spacing: 50) {
+            Text("Lucario")
+            Text("N.° 0448")
+        }
+        .bold()
+        .font(.system(size: 30))
+        
+        
+        //Description and image
+        HStack {
+            Image("placeholder")
+                .resizable()
+                .frame(width: 150, height: 150)
+            Text("Un Lucario bien entrenado es capaz de emitir ondas que le permiten percibir la presencia y los sentimientos de un individuo incluso a 1 km.")
+        }
+    }
+}
