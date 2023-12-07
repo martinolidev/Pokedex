52910830e5bbf24420b2a75f6e3925bb468c79e7
Martin Martinez <mtz.martin@proton.me>
2023-12-07 15:57:48 -0600

    Code aded

--- Pokedex/GenInfoView.swift
+++ Pokedex/GenInfoView.swift
@@ -9,7 +9,33 @@ import SwiftUI
 
 struct GenInfoView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("General Information")
+                .bold()
+                .font(.system(size: 30))
+            
+            RoundedRectangle(cornerRadius: 16)
+                .frame(width: 410, height: 120)
+                .overlay(
+                    VStack(alignment: .leading) {
+                        HStack(spacing: 50) {
+                            Text("Height: ")
+                            Text("1,2m")
+                                .foregroundStyle(Color.black)
+                        }
+                        HStack(spacing: 50) {
+                            Text("Weight: ")
+                            Text("54,0Kg")
+                                .foregroundStyle(Color.black)
+                        }
+                    }
+                        .font(.system(size: 30))
+                        .foregroundStyle(Color.white)
+                        .bold()
+                )
+                .foregroundStyle(Color.cyan)
+        }
+
     }
 }
 
--- Pokedex/PokemonDetailView.swift
+++ Pokedex/PokemonDetailView.swift
@@ -10,34 +10,6 @@ import SwiftUI
 struct PokemonDetailView: View {
     var body: some View {
         VStack {
-            
-            VStack {
-                Text("General Information")
-                    .bold()
-                    .font(.system(size: 30))
-                
-                RoundedRectangle(cornerRadius: 16)
-                    .frame(width: 410, height: 120)
-                    .overlay(
-                        VStack(alignment: .leading) {
-                            HStack(spacing: 50) {
-                                Text("Height: ")
-                                Text("1,2m")
-                                    .foregroundStyle(Color.black)
-                            }
-                            HStack(spacing: 50) {
-                                Text("Weight: ")
-                                Text("54,0Kg")
-                                    .foregroundStyle(Color.black)
-                            }
-                        }
-                            .font(.system(size: 30))
-                            .foregroundStyle(Color.white)
-                            .bold()
-                    )
-                    .foregroundStyle(Color.cyan)
-            }
-            
             VStack {
                 let abilities = ["Steadfast", "Inner-focus", "Justified"]
                 Text("Abilities")
