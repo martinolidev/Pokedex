abaa5e63ad553f76ce7c2a79c68c88389275c320
Martin Martinez <mtz.martin@proton.me>
2023-12-07 12:08:18 -0600

    YEP, this looks better

--- Pokedex/PokemonDetailView.swift
+++ Pokedex/PokemonDetailView.swift
@@ -28,7 +28,29 @@ struct PokemonDetailView: View {
                 Text("Un Lucario bien entrenado es capaz de emitir ondas que le permiten percibir la presencia y los sentimientos de un individuo incluso a 1 km.")
             }
             
-            
+            VStack {
+                Text("Type")
+                    .bold()
+                    .font(.system(size: 30))
+                HStack {
+                    RoundedRectangle(cornerRadius: 16)
+                        .foregroundColor(.red)
+                        .overlay(
+                            Text("Fight")
+                                .foregroundStyle(Color.white)
+                                .bold()
+                        )
+                        .frame(width: 80, height: 30)
+                    RoundedRectangle(cornerRadius: 16)
+                        .foregroundStyle(Color.gray)
+                        .overlay(
+                            Text("Steel")
+                                .foregroundStyle(Color.white)
+                                .bold()
+                        )
+                        .frame(width: 80, height: 30)
+                }
+            }
         }
         .padding()
     }
