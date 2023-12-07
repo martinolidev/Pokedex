2dfc7411d35c204887ad76b63bc734e6689df921
Martin Martinez <mtz.martin@proton.me>
2023-12-07 15:53:31 -0600

    New file for the types

---
+++ Pokedex/TypesView.swift
@@ -0,0 +1,18 @@
+//
+//  TypesView.swift
+//  Pokedex
+//
+//  Created by Martin Martinez on 07/12/23.
+//
+
+import SwiftUI
+
+struct TypesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TypesView()
+}
