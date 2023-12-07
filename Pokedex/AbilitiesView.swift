6311258020d8c5750baff911345c0340f471525a
Martin Martinez <mtz.martin@proton.me>
2023-12-07 15:58:35 -0600

    Another file to separate everything

---
+++ Pokedex/AbilitiesView.swift
@@ -0,0 +1,18 @@
+//
+//  AbilitiesView.swift
+//  Pokedex
+//
+//  Created by Martin Martinez on 07/12/23.
+//
+
+import SwiftUI
+
+struct AbilitiesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AbilitiesView()
+}
