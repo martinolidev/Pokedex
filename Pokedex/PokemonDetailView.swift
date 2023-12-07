0aa51ed2d277d751e822c1321d5da931a228b575
Martin Martinez <mtz.martin@proton.me>
2023-12-07 15:52:52 -0600

    Removing the type view for another file

--- Pokedex/PokemonDetailView.swift
+++ Pokedex/PokemonDetailView.swift
@@ -10,31 +10,6 @@ import SwiftUI
 struct PokemonDetailView: View {
     var body: some View {
         VStack {
-            
-            //type info
-            VStack {
-                Text("Type")
-                    .bold()
-                    .font(.system(size: 30))
-                HStack {
-                    RoundedRectangle(cornerRadius: 16)
-                        .foregroundColor(.red)
-                        .overlay(
-                            Text("Fight")
-                                .foregroundStyle(Color.white)
-                                .bold()
-                        )
-                        .frame(width: 80, height: 30)
-                    RoundedRectangle(cornerRadius: 16)
-                        .foregroundStyle(Color.gray)
-                        .overlay(
-                            Text("Steel")
-                                .foregroundStyle(Color.white)
-                                .bold()
-                        )
-                        .frame(width: 80, height: 30)
-                }
-            }
             //abilities
             VStack {
                 Text("Gender's")
