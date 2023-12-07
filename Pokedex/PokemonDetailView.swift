cae9e1139237562ef8b1251ab9073b34d2a7a558
Martin Martinez <mtz.martin@proton.me>
2023-12-07 12:04:47 -0600

    Looks better this way lol

--- Pokedex/PokemonDetailView.swift
+++ Pokedex/PokemonDetailView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct PokemonDetailView: View {
     var body: some View {
         VStack {
+            
+            //name and id
             HStack(spacing: 50) {
                 Text("Lucario")
                 Text("N.° 0448")
@@ -17,39 +19,16 @@ struct PokemonDetailView: View {
             .bold()
             .font(.system(size: 30))
             
-            RoundedRectangle(cornerRadius: 16)
-                .overlay(
-                    HStack(spacing: 20) {
-                        Image("placeholder")
-                            .resizable()
-                            .foregroundColor(.white)
-                            .frame(width: 100, height: 100)
-                        Text("Dicen que, como es capaz de detectar auras, puede percibir a sus rivales aunque no los vea.")
-                            .foregroundColor(.white)
-                            .bold()
-                            .font(.system(size: 17))
-                    }.padding()
-                )
-                .frame(height: 150)
-                .foregroundColor(.red)
             
-            RoundedRectangle(cornerRadius: 16)
-                .overlay(
-                    HStack {
-                        VStack {
-                            Text("Height")
-                                .foregroundStyle(Color.white)
-                            Text("1,2m")
-                                .foregroundStyle(Color.black)
-                            
-                            Text("Weight")
-                                .foregroundStyle(Color.white)
-                            Text("54,0 kg")
-                                .foregroundStyle(Color.black)
-                        }
-                    }
-                )
-                .foregroundColor(.blue)
+            //Description and image
+            HStack {
+                Image("placeholder")
+                    .resizable()
+                    .frame(width: 150, height: 150)
+                Text("Un Lucario bien entrenado es capaz de emitir ondas que le permiten percibir la presencia y los sentimientos de un individuo incluso a 1 km.")
+            }
+            
+            
         }
         .padding()
     }
